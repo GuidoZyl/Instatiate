8fc24b3bae35b3f77f0945947d31fa105394e972
GuidoZyl <guidozyl@gmail.com>
2022-05-04 15:49:50 -0300

    Initial commit

---
+++ Assets/CloneObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour
+{
+    public GameObject cubito;
+
+    public void ClonarCubo()
+    {
+        int i = 0;
+        while (i < 3)
+        {
+            Instantiate(cubito);
+            i++;
+        }
+    }
+}
